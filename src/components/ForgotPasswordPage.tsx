7a303d89ef7dbd6fe3b8fcbb9117d02f1aa3bae8
Yoshita Sharma <yoshita.as.sharma@gmail.com>
2025-10-04 14:20:21 -0700

    Updated ForgotPasswordPage.tsx

--- src/components/ForgotPasswordPage.tsx
+++ src/components/ForgotPasswordPage.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Mail, Send, CheckCircle, Shield, Lock, Key, ArrowLeft } from 'lucide-react';
+import { supabase } from './supabaseClient'
+
 
 interface FormData {
   email: string;

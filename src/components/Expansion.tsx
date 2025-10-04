e17a523153c04ebe963ead1c80dff161e658af2d
Yoshita Sharma <yoshita.as.sharma@gmail.com>
2025-10-04 15:31:40 -0700

    Updated Expansion.tsx

--- src/components/Expansion.tsx
+++ src/components/Expansion.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TrendingUp } from 'lucide-react';
-import { supabase } from './supabaseClient'
+import { supabase } from '../supabaseClient'
 
 
 export function Expansion() {

8581885d882f654302b638a0f4362dfb5deeb229
Yoshita Sharma <yoshita.as.sharma@gmail.com>
2025-10-04 15:32:17 -0700

    Updated SamplePrograms.tsx

--- src/components/SamplePrograms.tsx
+++ src/components/SamplePrograms.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ExternalLink, Clock } from 'lucide-react';
-import { supabase } from './supabaseClient'
+import { supabase } from '../supabaseClient'
 
 
 export function SamplePrograms() {

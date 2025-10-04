de574384849dd518615938ce5b361e59a02db9ff
Yoshita Sharma <yoshita.as.sharma@gmail.com>
2025-10-04 14:18:15 -0700

    Updated AboutPage.tsx

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Heart, Shield, Users, Search, FileText, CheckCircle, DollarSign } from 'lucide-react';
+import { supabase } from './supabaseClient'
+
 
 export function AboutPage() {
   return (

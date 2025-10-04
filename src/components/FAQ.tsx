59b325c2896dbeb075a736edc6e1a57f8e673102
Yoshita Sharma <yoshita.as.sharma@gmail.com>
2025-10-04 15:31:43 -0700

    Updated FAQ.tsx

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { ChevronDown, ChevronUp } from 'lucide-react';
-import { supabase } from './supabaseClient'
+import { supabase } from '../supabaseClient'
 
 
 export function FAQ() {

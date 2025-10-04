244bd10b9fe1f31e78be63b19758dfe1a4e98944
Yoshita Sharma <yoshita.as.sharma@gmail.com>
2025-10-04 14:20:16 -0700

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowDown, UserPlus } from 'lucide-react';
+import { supabase } from './supabaseClient'
+
 
 export function Hero() {
   const scrollToSearch = () => {

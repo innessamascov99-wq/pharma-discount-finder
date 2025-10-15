c0895ba7cd9987b02587513a3013bd44ca31b43a
innessamascov99-wq <innessa.mascov99@gmail.com>
2025-10-16 02:40:23 +0400

    Updated Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -229,7 +229,7 @@ export const Contact: React.FC = () => {
                           href="mailto:pharmadiscountfinder@gmail.com"
                           className="text-blue-600 hover:text-blue-700 font-semibold hover:underline transition-colors"
                         >
-                          
+                          pharmadiscountfinder@gmail.com
                         </a>
                       </div>
                     </div>
